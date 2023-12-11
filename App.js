cb54bb43cb96136fb07cf975523803b08b60102b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-11 20:32:15 +0530

    updated app

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
