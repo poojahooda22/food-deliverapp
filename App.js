7bd3bafeffc2ba51a2c4efd09917b9ed5a31e384
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-11 19:19:03 +0530

    updated stack in App

--- App.js
+++ App.js
@@ -2,25 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   return (
     <NavigationContainer>
       <TailwindProvider>
-        <View style={styles.container}>
-          <Text className="text-red-500">Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
