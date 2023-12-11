5f4b51db2b5ca74202185caa35d5eac07a925612
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-11 22:35:57 +0530

    updated app

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TailwindProvider } from 'tailwindcss-react-native';
-import './tailwind.css';
+
 import HomeScreen from './screens/HomeScreen';
 
 
