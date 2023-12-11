4f24b9f768e6e8d01b470b428a7a2e99af36c811
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-11 19:20:22 +0530

    updated app

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
