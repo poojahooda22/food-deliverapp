fec24230f39372aca09a09561ee56d2ecfe3896c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-11 19:15:34 +0530

    updated app

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TailwindProvider>
+        <View style={styles.container}>
+          <Text className="text-red-500">Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
 
