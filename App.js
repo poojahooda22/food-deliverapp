67f578c13bd64e0f77ae15dd30c9e09d9e0debf2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-12 16:01:01 +0530

    updated app

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <TailwindProvider>
-      <View style={styles.container}>
-        <Text style="text-center text-white text-2xl">
-          Hello World
-        </Text>
-      </View>
-    </TailwindProvider>
-  )
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
