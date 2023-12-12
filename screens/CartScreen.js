223195a722cc81b0b72846426cc28f89a7bf135b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-12 15:45:00 +0530

    create new screen

---
+++ screens/CartScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+function CartScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>There is no order yet!</Text>
+      </View>
+    );
+  }
+
+export default HomeScreen;
