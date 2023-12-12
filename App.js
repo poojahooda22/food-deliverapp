9da261d008c43301fee2f9eeff341e59a51a835b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-12 19:56:53 +0530

    updated

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
-        <Stack.Screen/>
+        <Stack.Screen name="Settings"/>
       </TailwindProvider>
     </NavigationContainer>
   );
