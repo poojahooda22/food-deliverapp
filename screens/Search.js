991296981149f90a22e248a915221918943a3ad6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-13 20:27:03 +0530

    updated

--- screens/Search.js
+++ screens/Search.js
@@ -4,10 +4,12 @@ import React from 'react'
 const Search = () => {
   return (
     <View className="flex-row items-center space-x-2 mx-4">
-          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-          <SearchIcon color="gray" size={20} />
-            
-          </View>
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+            <SearchIcon color="gray" size={20} />
+            <TextInput
+                placecholder="Restaurants and cuisines"
+            />
+        </View>
     </View> 
   )
 }
