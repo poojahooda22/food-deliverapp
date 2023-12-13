99ae0ae84af303c4bace3588adaf70fb6237dbdb
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-13 20:26:01 +0530

    create search

---
+++ screens/Search.js
@@ -0,0 +1,14 @@
+import { View, Text, TextInput } from 'react-native'
+import React from 'react'
+
+const Search = () => {
+  return (
+    <View className="flex-row items-center space-x-2 mx-4">
+          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+            
+          </View>
+    </View> 
+  )
+}
+
+export default Search;
